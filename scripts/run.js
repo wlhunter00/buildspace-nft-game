6fb95766052e64613fbf06ac909bc106ef918a01
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2022-02-15 01:22:13 -0600

    setup basic files for project

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy();
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+runMain();
